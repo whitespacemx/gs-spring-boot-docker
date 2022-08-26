e1e2ae5f77ebe7f1412a88bb027770bab8438b0f
Spencer Sanchez <spencer.sanchez@whitespace.com.mx>
2022-08-26 11:04:40 -0500

    add ingress

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	@RequestMapping("/")
+	@RequestMapping("/demo-path")
 	public String home() {
 		return "Hello Docker World";
 	}
